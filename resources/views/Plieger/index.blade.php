94d628f6e3d9a6b04636d98bc7eb4fcff9d0e5e7
Sabim Prajapati <prajapati.sabim@gmail.com>
2021-08-09 20:11:16 +0545

    Plieger dashboard

--- resources/views/Plieger/index.blade.php
+++ resources/views/Plieger/index.blade.php
@@ -31,15 +31,21 @@
 <!-- END PAGE TITLE-->
 <!-- END PAGE HEADER-->
 <!-- BEGIN DASHBOARD STATS 1-->
+
 <div class="row">
     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
         <a class="dashboard-stat dashboard-stat-v2 blue" href="#">
             <div class="visual">
                 <i class="fa fa-tasks"></i>
             </div>
-            <div class="details">
-                <div class="number">
-                    <span data-counter="counterup" data-value="<?php echo $total_assigned_task ?>">0</span>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $total_assigned_task_gis ?>">0</span>
                 </div>
                 <div class="desc"> Total Assigned Task </div>
             </div>
@@ -50,9 +56,14 @@
             <div class="visual">
                 <i class="fa fa-bar-chart-o"></i>
             </div>
-            <div class="details">
-                <div class="number">
-                    <span data-counter="counterup" data-value="<?php echo $remaining_task ?>">0</span></div>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $remaining_task_gis ?>">0</span></div>
                 <div class="desc"> Remaining Task </div>
             </div>
         </a>
@@ -62,9 +73,14 @@
             <div class="visual">
                 <i class="fa fa-file-text"></i>
             </div>
-            <div class="details">
-                <div class="number">
-                    <span data-counter="counterup" data-value="<?php echo $team_leader_review ?>">0</span>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $team_leader_review_gis ?>">0</span>
                 </div>
                 <div class="desc">TeamLeader Reviewing Files </div>
             </div>
@@ -75,9 +91,14 @@
             <div class="visual">
                 <i class="fa fa-file-text"></i>
             </div>
-            <div class="details">
-                <div class="number">
-                    <span data-counter="counterup" data-value="<?php echo $team_supervisor_review ?>">0</span>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $team_supervisor_review_gis ?>">0</span>
                 </div>
                 <div class="desc">Supervisor Reviewing Files </div>
             </div>
@@ -88,9 +109,14 @@
             <div class="visual">
                 <i class="fa fa-file-text"></i>
             </div>
-            <div class="details">
-                <div class="number">
-                    <span data-counter="counterup" data-value="<?php echo $total_finished ?>">0</span>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $total_finished_gis ?>">0</span>
                 </div>
                 <div class="desc"> Total Finished </div>
             </div>
@@ -101,9 +127,14 @@
             <div class="visual">
                 <i class="fa fa-upload"></i>
             </div>
-            <div class="details">
-                <div class="number"> 
-                    <span data-counter="counterup" data-value="<?php echo $total_uploaded ?>">0</span></div>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span> 
+                    <span data-counter="counterup" data-value="<?php echo $total_uploaded_gis ?>">0</span></div>
                 <div class="desc"> Total Uploaded </div>
             </div>
         </a>
@@ -113,25 +144,15 @@
             <div class="visual">
                 <i class="fa fa-warning"></i>
             </div>
-            <div class="details">
-                <div class="number"> 
-                    <span data-counter="counterup" data-value="<?php echo $issues_remaining ?>">0</span></div>
-                <div class="desc"> Issues Task Remaining </div>
-            </div>
-        </a>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
-        <a class="dashboard-stat dashboard-stat-v2 blue" href="#">
-            <div class="visual">
-                <i class="fa fa-comments"></i>
-            </div>
-            <div class="details">
-                <div class="number">
-                    <span data-counter="counterup" data-value="<?php echo $plieger_send ?>">0</span>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
                 </div>
-                <div class="desc">Send For Plieger Review</div>
+                <div class="number" style="padding-top: 10px;"> 
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $issues_remaining_gis ?>">0</span></div>
+                <div class="desc"> Issues Task Remaining </div>
             </div>
         </a>
     </div>
@@ -140,10 +161,32 @@
             <div class="visual">
                 <i class="fa fa-bar-chart-o"></i>
             </div>
-            <div class="details">
-                <div class="number">
-                    <span data-counter="counterup" data-value="<?php echo $plieger_remaining ?>">0</span></div>
-                <div class="desc"> Remaining Plieger Review </div>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $plieger_remaining_gis ?>">0</span></div>
+                <div class="desc"> Remaining Henk Review </div>
+            </div>
+        </a>
+    </div> 
+    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+        <a class="dashboard-stat dashboard-stat-v2 green" href="#">
+            <div class="visual">
+                <i class="fa fa-bar-chart-o"></i>
+            </div>
+            <div class="details" style="width: 60%">
+                <div class="project" style="padding-top: 9px;text-align: right;font-size: 15px;color: white;"> 
+                    <span style="float: left;">DataCircle</span>
+                    <span>GIS</span>
+                </div>
+                <div class="number" style="padding-top: 10px;">
+                    <span style="float: left;" data-counter="counterup" data-value="<?php echo $total_assigned_task_dc ?>">0</span>
+                    <span data-counter="counterup" data-value="<?php echo $plieger_feedback_gis ?>">0</span></div>
+                <div class="desc">Henk Feedback </div>
             </div>
         </a>
     </div>    
